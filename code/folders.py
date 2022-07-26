374a80e121e4873f91f7e697be0e0b29a54fe4c4
José Ignacio González Rojas <65085307+jigonr@users.noreply.github.com>
2022-07-26 13:16:29 -0600

    commuting zones

---
+++ code/folders.py
@@ -0,0 +1,45 @@
+import os
+
+# Folder structure
+
+CODEDIR = "codes"
+DATADIR = "data"
+RAW_DATA = os.path.join(DATADIR, "00-raw_data")
+INTERMEDIATE_DATA = os.path.join(DATADIR, "01-intermediate_data")
+FINAL_DATA = os.path.join(DATADIR, "02-final_data")
+OUTDIR = "output"
+FIGURES_OUTPUT = os.path.join(OUTDIR, "01-figures")
+FIGURES_MAIN_TEXT_OUTPUT = os.path.join(FIGURES_OUTPUT, "01-main_text")
+FIGURES_APPENDIX_OUTPUT = os.path.join(FIGURES_OUTPUT, "02-appendix")
+TABLES_OUTPUT = os.path.join(OUTDIR, "02-tables")
+TABLES_MAIN_TEXT_OUTPUT = os.path.join(TABLES_OUTPUT, "01-main_text")
+TABLES_APPENDIX_OUTPUT = os.path.join(TABLES_OUTPUT, "02-appendix")
+
+# Input files
+
+## Raw Data
+
+### Census 2011
+census2011_file = os.path.join(RAW_DATA, r"CR_CENSO2011_Muestra10%_NBI.sav")
+
+### Costa Rican map
+costa_rica_shapefile = os.path.join(RAW_DATA, "cri_admbnda_adm2_2021_WGS_84.shp")
+
+### Region concordance table
+region_concordance_file = os.path.join(RAW_DATA, "region_concordance.xlsx")
+
+### Costa Rican Roads Network Dataset
+costa_rican_roads_file = os.path.join(RAW_DATA, "costa-rica-latest.osm")
+
+### ENAHO 2020
+enaho2020_file = os.path.join(RAW_DATA, "ENAHO 2020.sav")
+
+### ENAHO 2021
+enaho2021_file = os.path.join(RAW_DATA, "ENAHO 2021.sav")
+
+## Intermediate Data
+costa_rican_commuting_zones_file = os.path.join(INTERMEDIATE_DATA, "czcr.xlsx")
+
+## Final Data
+costa_rican_municipalities_distance_matrix = os.path.join(FINAL_DATA, "distances.xlsx")
+
